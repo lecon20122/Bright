122925eff9f5c9a8dd5a70b834cdd93afdd157a4
lecon20122 <lecon20122@gmail.com>
2022-04-15 17:26:16 +0200

    first commit

---
+++ resources/views/includes/nav.blade.php
@@ -0,0 +1,41 @@
+<nav class="navbar navbar-expand-md">
+    <div class="container">
+        <a class="navbar-brand brand" href="#"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto d-flex flex-row-reverse">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">الصفحة الرئيسية</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"> للكشف</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">اعرفني</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">تسجيل</a>
+                </li>
+
+            </ul>
+        </div>
+    </div><!-- container -->
+</nav>
+<header class="py-2">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-sm-6">
+                <img src="{{ asset('images/lagog ff.jfif') }}" alt="" srcset="">
+            </div>
+
+            <div class="col-sm-6 d-flex justify-content-center">
+                <h1 class="my-3 text-uppercase display-4 font-weight-bold ">WE SPECIAL</h1>
+            </div>
+
+        </div><!-- row -->
+    </div><!-- container -->
+</header>
---
+++ resources/views/website.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.home')
+
+@section('content')
+<div>
+</div>
+@endsection
